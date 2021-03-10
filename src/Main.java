de34550654e619e36e2b8501015dc939bea118d9
AlexeyVFrolov <isbase@mail.ru>
2021-03-10 20:55:24 +0300

    Code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
+    System.out.print("Please, enter the ticket price in rubles ");
+    long TicketPrice = (long)(in.nextDouble() * 100); // ticket price, kopecks
+    System.out.println(String.format("Your free miles accrued: %s", TicketPrice/2000)); // 1 mile per 20 ruble
     }
 }
