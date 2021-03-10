b7c1c2483a1111f8d9e79bbe8b1a82e86a5d4682
AlexeyVFrolov <isbase@mail.ru>
2021-03-10 22:59:49 +0300

    Code reformated, local variables names changed

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    System.out.print("Please, enter the ticket price in rubles ");
-    long TicketPrice = (long)(in.nextDouble() * 100); // ticket price, kopecks
-    System.out.println(String.format("Your free miles accrued: %s", TicketPrice/2000)); // 1 mile per 20 ruble
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please, enter the ticket price in rubles ");
+        long ticketPrice = (long) (in.nextDouble() * 100); // ticket price, kopecks
+        System.out.println(String.format("Your free miles accrued: %s", ticketPrice / 2000)); // 1 mile per 20 ruble
     }
 }
